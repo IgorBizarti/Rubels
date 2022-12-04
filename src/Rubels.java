7dcd60251a2aef8e48a05e2bda019368ccd64316
Igor Byazrov <fbi1978@mail.ru>
2022-12-04 12:15:12 +0300

    Changed

---
+++ src/Rubels.java
@@ -0,0 +1,21 @@
+public class Rubels {
+
+    public static void main(String[] args) {
+
+        int baseAmount = 100;
+        int addAmount = 1020;
+
+        int bonus;
+
+        if (addAmount >= 1020) {
+            bonus = addAmount / 100;
+        } else {
+            bonus = 0;
+        }
+        int balance = addAmount / baseAmount * bonus;
+
+        System.out.println(" Вам начислено " + bonus + " Бонусных рублей");
+
+
+    }
+}
